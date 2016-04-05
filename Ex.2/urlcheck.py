81eeb662927948b21b312c7f09c53d86c820b984
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2016-04-05 14:18:18 +0200

    Reformat tabs

--- Ex.2/urlcheck.py
+++ Ex.2/urlcheck.py
@@ -7,16 +7,15 @@ import sys
 
 if len(sys.argv) > 1 :
 
-	checkemail = re.compile(r"^(http|https|ftp|file)+://")
-	url = str(sys.argv[1])
-	
-	if checkemail.match(url) is None :
-		print("Unknown URL format")
-	else :
-		try :
-			urllib.request.urlopen(url).getcode()
-			print("The URL is valid")
-		except :
-			print("Impossible to load URL")
+    checkemail = re.compile(r"^(http|https|ftp|file)+://")
+    url = str(sys.argv[1])
 
+    if checkemail.match(url) is None :
+        print("Unknown URL format")
+    else :
+	try :
+	    urllib.request.urlopen(url).getcode()
+                print("The URL is valid")
+	except :
+		print("Impossible to load URL")
 
