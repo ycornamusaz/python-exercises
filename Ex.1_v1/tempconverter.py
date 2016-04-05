0f312fc46ab31fa2c41cd13d0cf846be3c8652ac
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2016-04-05 09:13:42 +0200

    Upgrade tempconverter.py to v2

--- Ex.1_v1/tempconverter.py
+++ Ex.1_v1/tempconverter.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3.4
 # -*- coding: UTF-8 -*-
 
-number = input('Entrez une température : ')
-unit = input('Convertir vers des degrés (C)elsius ou (F)ahrenheit ? ')
+import sys
+
+if len(sys.argv) != 3 :
+	number = input('Entrez une température : ')
+	unit = input('Convertir vers des degrés (C)elsius ou (F)ahrenheit ? ')
+else :
+	number = sys.argv[1]
+	unit = sys.argv[2]
+
+
 if unit == "F" :
 	output = (9.0/5.0)*float(number)+32
 elif unit == "C" :
