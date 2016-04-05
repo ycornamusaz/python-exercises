9438f17714f0826deece3e2b2e41e5630c043361
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2016-04-05 15:15:04 +0200

    Comment and set number's type at source

--- Ex.1/tempconverter.py
+++ Ex.1/tempconverter.py
@@ -3,20 +3,26 @@
 
 import sys
 
+# Check if arguments are present
 if len(sys.argv) != 3 :
-    number = input('Entrez une température : ')
+    # If no, question
+    number = float(input('Entrez une température : '))
     unit = input('Convertir vers des degrés (C)elsius ou (F)ahrenheit ? ')
 else :
-    number = sys.argv[1]
+    # If yes, use arguments
+    number = float(sys.argv[1])
     unit = sys.argv[2]
 
-
+# Check conversion type and calcul
 if unit == "F" :
-    output = (9.0/5.0)*float(number)+32
+    output = (9.0/5.0)*number+32
 elif unit == "C" :
-    output = (5.0/9.0)*(float(number)-32)
+    output = (5.0/9.0)*(number-32)
 else :
+    # If conversion type isn't correct, dispay an error
     print('Veuillez entrer une unité correcte !')
-
+    exit()
+i
+# Print result
 print('Résultat : ' + str(round(float(output), 2)))
 
