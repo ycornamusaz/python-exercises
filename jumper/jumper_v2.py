78e5cba305e11a9611a3746ee7ed46ca264a5de0
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2016-04-12 14:00:49 +0200

    Add class Buton

--- jumper/jumper_v2.py
+++ jumper/jumper_v2.py
@@ -55,3 +55,28 @@ class Ground(pygame.sprite.Sprite):
         self.rect.y = var.HEIGHT - 16 - 94
         self.rect.x = 32
 
+class Buton(pygame.sprite.Sprite):
+
+    def __init__(self, text, color):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        self.image = pygame.image.load("PNG/Environment/ground_grass.png").convert()
+        self.image.set_colorkey(BLACK)
+
+        self.rect = self.image.get_rect()
+        self.width = 380
+        self.height = 94
+        self.jump = 0
+        self.speed = 0
+        self.rect.x = (var.width/2 - self.width/2)
+        self.rect.y = 32
+
+        self.text = text
+        self.color = color
+        self.font = pygame.font.SysFont("Ubuntu", 25)
+        self.textSurf = font.render(self.text, 1, self.color)
+        self.text_width = self.textSurf.get_width()
+        self.text_height = self.textSurf.get_height()
+        self.image.blit(self.textSurf, ((self.width/2 - self.text_width/2), (self.height/2 - self.text_height/2)))
+
