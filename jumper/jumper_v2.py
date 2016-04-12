4e067149b2d762038f63508f6ae6e1f1d93fd1da
Yoël Cornamusaz <yoel.cornamusaz@camptocamp.com>
2016-04-12 13:44:42 +0200

    Add function color

--- jumper/jumper_v2.py
+++ jumper/jumper_v2.py
@@ -7,5 +7,11 @@ def var(self):
     self.WIDTH = 1300
     self.HEIGHT = 1000
 
-
+def color(self):
+    ## Define some colors
+    self.BLACK    = (   0,   0,   0)
+    self.WHITE    = ( 255, 255, 255)
+    self.BLUE     = (   0,   0, 255)
+    self.GREEN    = (   0, 255,   0)
+    self.RED      = ( 255,   0,   0)
 
